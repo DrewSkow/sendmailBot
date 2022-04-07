76aebeb2c556d87c5200e1d21fe526b30ebaf996
DrewSkow <andrusha.skorlupin@gmail.com>
2022-04-07 14:12:45 +0300

    fixet switch button

--- js.js
+++ js.js
@@ -1,11 +1,25 @@
 const btn = document.getElementById("btn");
-let lsD = true;
+const stor = chrome.storage.sync;
 
-const stor = chrome.storage.sync
+const setState = (v) => {
+    if (v==true){
+        btn.children[0].checked=true;
+    } else {btn.children[0].checked=false}
+}
+
+
+stor.get("switch", v => setState(v.switch));
+
+const switcher = (v) => {
+    if(v==undefined){
+        stor.set({switch:true});
+    } else if (v==true){
+        stor.set({switch:false});
+    } else if (v==false) {stor.set({switch:true});}
+}
 
 btn.onclick = () => {
-    stor.get("switch").then(v=>{lsD=v===false?true:false});
-    stor.set({switch:lsD});
+    stor.get("switch", v => switcher(v?.switch))
 }
 
 
